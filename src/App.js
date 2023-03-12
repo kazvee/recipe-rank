c7b4f47bf87d1b2fbd067ae701818c09516365df
kazvee <kayvee123@gmail.com>
2023-03-12 10:48:26 -0600

    Divide interface into components

--- src/App.js
+++ src/App.js
@@ -2,18 +2,41 @@ import React from 'react';
 import './styles.css';
 
 function App() {
+  const appTitle = 'Recipe Rank';
   return (
-    <header class='header'>
-      <div class='logo'>
-        <img
-          src='https://img.icons8.com/external-bearicons-outline-color-bearicons/64/null/external-Recipe-Book-cooking-bearicons-outline-color-bearicons.png'
-          alt='Recipe Rank Logo'
-        />
-        <h1>Welcome to Recipe Rank!</h1>
-      </div>
-      <button class='btn btn-large btn-open'>Share a recipe</button>
-    </header>
+    <>
+      {/* Header */}
+      <header className='header'>
+        <div className='logo'>
+          <img
+            src='https://img.icons8.com/external-bearicons-outline-color-bearicons/64/null/external-Recipe-Book-cooking-bearicons-outline-color-bearicons.png'
+            alt='Recipe Rank Logo'
+          />
+          <h1>{appTitle}</h1>
+        </div>
+        <button className='btn btn-large btn-open'>Share a recipe</button>
+      </header>
+
+      <NewRecipeForm />
+
+      <main className='main'>
+        <CategoryFilter />
+        <RecipeList />
+      </main>
+    </>
   );
+}
+
+function NewRecipeForm() {
+  return <form className='recipe-form'>Recipe Form</form>;
+}
+
+function CategoryFilter() {
+  return <aside>Category Filter</aside>;
+}
+
+function RecipeList() {
+  return <section>Recipe List</section>;
 }
 
 export default App;
